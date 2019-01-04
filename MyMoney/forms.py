05124a838605109551a2f7a75bba71824c9ffc02
Michaël Paulon <michael@paulon.org>
2019-01-04 21:20:55 +0100

    remove login form

--- MyMoney/forms.py
+++ MyMoney/forms.py
@@ -3,11 +3,6 @@ from django.contrib.auth.models import User
 
 import MyMoney.models as models
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["username", "password"]
-
 class ShareForm(forms.ModelForm):
     class Meta:
         model = models.Share
