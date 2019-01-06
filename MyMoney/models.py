06f469eb15b8bdac2e3022b6a34db456292cf8f9
Maxime Bombar <bombar@crans.org>
2019-01-06 01:46:48 +0100

    Don't cross the streams https://www.youtube.com/watch?v=wyKQe_i9yyo

--- MyMoney/models.py
+++ MyMoney/models.py
@@ -14,22 +14,22 @@ class Currency(models.Model):
     def __str__(self):
         return "{} ({})".format(self.name, self.symbol)
 
-class Share(models.Model):
-    value = models.DecimalField(decimal_places=2, max_digits=10)
-    owner = models.ForeignKey(User, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return "{} {}".format(self.owner, self.value)
-
 class Expense(models.Model):
     name = models.CharField(max_length=50)
     origin = models.ForeignKey(User, on_delete=models.PROTECT)
     group = models.ForeignKey(Group, on_delete=models.PROTECT)
     description = models.TextField()
-    shares = models.ManyToManyField(Share)
     value = models.DecimalField(decimal_places=2, max_digits=10)
     currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
 
 
     def __str__(self):
         return "{}-{} ({}) {} {}".format(self.group, self.name, self.origin, self.value, self.currency)
+
+class Share(models.Model):
+    value = models.DecimalField(decimal_places=2, max_digits=10)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    expense = models.ForeignKey(Expense, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{} {}".format(self.owner, self.value)
