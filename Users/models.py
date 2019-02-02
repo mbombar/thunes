89dc2928cd49e562c8b6bf1c4f80b86ed7ce16e1
Maxime Bombar <bombar@crans.org>
2019-02-02 11:37:39 +0100

    Ajoute un modèle Profile pour géréer des préférences sur les utilisateurs

--- Users/models.py
+++ Users/models.py
@@ -5,3 +5,14 @@ from django.db import models
 
 
 from django.contrib.auth.models import Group, User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    favorite_groups = models.ManyToManyField(
+        blank=True,
+        to='Users.Group',
+        related_name='favorite_groups')
+
+class Group(Group):
+    class Meta:
+        proxy = True
