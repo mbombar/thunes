c4e320a00a66d621266d7e0bf4297bf62dd589c6
Michaël Paulon <michael@paulon.org>
2019-02-24 17:13:31 +0100

    edit groups

--- Users/forms.py
+++ Users/forms.py
@@ -16,7 +16,13 @@ class UserCreationFormWithEmail(UserCreationForm):
 
 class GroupCreateOrEditForm(forms.ModelForm):
     """Formulaire de création ou l'édition d'un groupe"""
-
+    def __init__(self, *args, **kwargs):
+        super(GroupCreateOrEditForm, self).__init__(*args, **kwargs)
+        try:
+            self.fields['members'].initial = self.instance.user_set.all()
+            self.fields['name'].widget.attrs['readonly'] = 'true'
+        except:
+            pass
     members = ModelMultipleChoiceField(queryset = User.objects.all(), widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Group
