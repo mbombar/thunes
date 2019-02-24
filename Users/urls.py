ab98df53a77969d416ded0ab61c8ee96c33ff150
Michaël Paulon <michael@paulon.org>
2019-02-24 17:14:08 +0100

    edit groups

--- Users/urls.py
+++ Users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("users/", views.index_users, name='users'),
     path("groups/", views.index_groups, name='groups'),
     path("create/", views.create_user, name="create_user"),
-    path("groups/create/", views.create_group, name="create_group"),
+    path("groups/create/", views.create_edit_group, name="create_group"),
+    path("groups/edit/<gid>/", views.create_edit_group, name="edit_group"),
     ]
