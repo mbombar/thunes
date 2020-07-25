7d5d626f742ca9e7bc727f3cd96714b3132483b6
Maxime Bombar <bombar@crans.org>
2020-07-25 16:09:36 +0200

    Formulaire d'édition de dépense. Ouais ouais ça fait 18 mois qu'on attend la feature. Et alors :p ?

--- MyMoney/urls.py
+++ MyMoney/urls.py
@@ -7,6 +7,7 @@ app_name = "MyMoney"
 urlpatterns = [
     path("<gid>/balance/", views.show_balance, name="balance"),
     path("<gid>/expense/new/", views.new_expense, name="new-expense"),
+    path("<gid>/expense/<pk>/edit", views.edit_expense, name="edit-expense"),
     path("<gid>/expense/history/", views.index_expense, name="index-expense"),
     path("<gid>/repay/<user1>/<user2>", views.rembourser, name="rembourser"),
 ]
