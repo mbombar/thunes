d093df48a5a68714e3c508744ce5e1a3f0fbbb83
Michaël Paulon <michael@paulon.org>
2020-08-25 00:05:38 +0200

    add fields to admin list

--- DjangoTools/tools.py
+++ DjangoTools/tools.py
@@ -2,6 +2,10 @@ from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 
+class ListModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super().__init__(model, admin_site)
 
 def autoregister(*app_list):
         """
@@ -11,6 +15,7 @@ def autoregister(*app_list):
         for app in app_list:
             for model in apps.get_app_config(app).get_models():
                 try:
-                    admin.site.register(model)
+                    admin.site.register(model, ListModelAdmin)
                 except AlreadyRegistered:
                     pass
+
