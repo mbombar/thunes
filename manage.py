49b4888d025dcf0d19d92ae951fdc337f1d38908
Maxime Bombar <bombar@crans.org>
2021-05-14 13:07:45 +0200

    s/MathoosHouse/Thunes

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MathoosHouse.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Thunes.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
